f922d9bce3fa9e1adae2f65d245df5face0c57c3
Austen Turner <austenpturner@msn.com>
2024-04-17 14:40:17 -0700

    use ejs to inject data from server to index page

--- app.js
+++ app.js
@@ -10,18 +10,32 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "first blog post",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "second blog post",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "third blog post",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create a new blog" });
 });
 
 // 404 page ** MUST GO LAST
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404" });
 });
