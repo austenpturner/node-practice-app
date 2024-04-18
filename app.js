c95ae24f1ed6fadf4355caaf55a7f62ec3f3c25c
Austen Turner <austenpturner@msn.com>
2024-04-18 08:43:01 -0700

    add middleware and style sheet

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -8,6 +9,24 @@ app.set("view engine", "ejs");
 
 // listen for requests
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+// EXAMPLE MIDDLEWARE
+// app.use((req, res, next) => {
+//   console.log("new request made: ");
+//   console.log("host: ", req.hostname);
+//   console.log("path: ", req.path);
+//   console.log("method: ", req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("in the next middleware");
+//   next();
+// });
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -35,7 +54,7 @@ app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
 });
 
-// 404 page ** MUST GO LAST
+// 404 page ** MUST GO LAST, will fire if none of the above cases match
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
